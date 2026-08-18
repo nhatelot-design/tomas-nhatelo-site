7626858a86e4afebd9f3c14289383c77f791a746
nhatelot-design <nhatelot@gmail.com>
2026-08-18 18:48:08 +0100

    Atualização das páginas de formação e cartões visuais

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// =========================================
+// MENU MOBILE
+// =========================================
+
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", function() {
+    navMenu.classList.toggle("active");
+});
+
+navMenu.querySelectorAll("a").forEach(function(link) {
+
+    link.addEventListener("click", function() {
+        navMenu.classList.remove("active");
+    });
+
+});
